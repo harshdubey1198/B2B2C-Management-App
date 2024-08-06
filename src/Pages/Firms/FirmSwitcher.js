17e840a15f349b6942c6e7cfe5eb9800dcaec1fd
rohitsaini27 <rohsaini8@gmail.com>
2024-08-06 16:39:00 +0530

    changes in localstorage amd make firms dynamic

--- src/Pages/Firms/FirmSwitcher.js
+++ src/Pages/Firms/FirmSwitcher.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const firms = [
-  { id: 1, name: 'Firm A', logo: 'https://res.cloudinary.com/harshdubey1198/image/upload/v1722844391/abstract-colorful-logo_1017-8753_qptgtx.avif' },
-  { id: 2, name: 'Firm B', logo: 'https://res.cloudinary.com/harshdubey1198/image/upload/v1722844388/business-logo_23-2147503133_sw5xjq.avif' },
-  { id: 3, name: 'Firm C', logo: 'https://res.cloudinary.com/harshdubey1198/image/upload/v1722844376/gradient-logo-with-abstract-shape_23-2148219550_mgiwzf.avif' },
-  { id: 4, name: 'Firm D', logo: 'https://res.cloudinary.com/harshdubey1198/image/upload/v1722844367/colorful-floral-logo_1025-262_ahzfec.avif' },
-];
+// const firms = [
+//   { id: 1, name: 'Firm A', logo: 'https://res.cloudinary.com/harshdubey1198/image/upload/v1722844391/abstract-colorful-logo_1017-8753_qptgtx.avif' },
+//   { id: 2, name: 'Firm B', logo: 'https://res.cloudinary.com/harshdubey1198/image/upload/v1722844388/business-logo_23-2147503133_sw5xjq.avif' },
+//   { id: 3, name: 'Firm C', logo: 'https://res.cloudinary.com/harshdubey1198/image/upload/v1722844376/gradient-logo-with-abstract-shape_23-2148219550_mgiwzf.avif' },
+//   { id: 4, name: 'Firm D', logo: 'https://res.cloudinary.com/harshdubey1198/image/upload/v1722844367/colorful-floral-logo_1025-262_ahzfec.avif' },
+// ];
+
+const firms = JSON.parse(localStorage.getItem("Firms")) || []
+console.log(firms, "firms")
 
 function FirmSwitcher({ selectedFirmId, onSelectFirm }) {
   const [dropdownOpen, setDropdownOpen] = React.useState(false);
@@ -20,14 +23,14 @@ function FirmSwitcher({ selectedFirmId, onSelectFirm }) {
         {firms.find(firm => firm.id === selectedFirmId)?.name || 'Select Firm'}
       </DropdownToggle>
       <DropdownMenu>
-        {firms.map(firm => (
+        {firms && firms.map(firm => (
           <DropdownItem
             key={firm.id}
             onClick={() => onSelectFirm(firm.id)}
             active={firm.id === selectedFirmId}
           >
             <img
-              src={firm.logo}
+              src={firm.imageUrl}
               alt={firm.name}
               className='img-fluid'
               style={{ maxWidth: '30px', marginRight: '10px' }}
