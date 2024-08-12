ee7a4cbbc99b5250c8404e9864e62957dcf0650e
HARSH DUBEY <amanad08052@gmail.com>
2024-08-12 12:19:22 +0530

    Updated pricing , maintenance , 404 page

--- src/Pages/Utility/Error404-Page.js
+++ src/Pages/Utility/Error404-Page.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
 const Error404 = () => {
-    document.title = "404 Error  | aaMOBee";
+    document.title = "404 Error | aaMOBee";
     return (
         <React.Fragment>
             <div className="my-5 pt-5">
@@ -17,8 +16,8 @@ const Error404 = () => {
                                         <h1 className="display-2 error-text fw-bold">4<i className="ri-ghost-smile-fill align-bottom text-primary mx-1"></i>4</h1>
                                     </div>
                                     <div>
-                                        <h4 className="text-uppercase mt-4">Sorry, page not found</h4>
-                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+                                        <h4 className="text-uppercase mt-4">Oops! Page Not Found</h4>
+                                        <p>It seems the page you’re looking for doesn’t exist or has been moved. Please check the URL and try again.</p>
                                         <div className="mt-4">
                                             <Link to="/" className="btn btn-primary"><i className="ri-arrow-left-line align-bottom mr-2"></i>Back to Home</Link>
                                         </div>
@@ -29,7 +28,6 @@ const Error404 = () => {
                     </Container>
                 </div>
             </div>
-
         </React.Fragment>
     );
 }
