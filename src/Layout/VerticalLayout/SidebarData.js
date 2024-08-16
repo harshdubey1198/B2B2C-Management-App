2e877f790b4270717f3b33057f336e342aed1f87
HARSH DUBEY <amanad08052@gmail.com>
2024-08-16 11:14:47 +0530

    Firm Admin sidebar added

--- src/Layout/VerticalLayout/SidebarData.js
+++ src/Layout/VerticalLayout/SidebarData.js
@@ -168,6 +168,86 @@ export const masterAdminSidebarData = () => [
 
       }
   ];
+  export const firmAdminSidebarData =()=>[
+    {
+      label: "Dashboard",
+      icon: "mdi mdi-home-variant-outline",
+      url: "/dashboard",
+    },
+    {
+      label:"Companies",
+      icon:"mdi mdi-account-multiple-outline",
+      subItem: [
+        { sublabel: "Firms", link: "/firms" },
+        // { sublabel: "Manage Firm", link: "/manage-firm" },
+        { sublabel: "Create Firm", link: "/create-firm" },
+        { sublabel: "Switch Firm", link: "/Switch-firm" },
+        { sublabel: "Firm Setting", link: "/firms-setting" }
+      ],
+    },
+    {
+      label:"Pricing",
+      icon:"mdi mdi-cash-multiple",
+      url:"/pages-pricing",
+    },
+    {
+      label: "Invoicing",
+      icon: "mdi mdi-cash-multiple",
+      subItem: [
+        { sublabel: "Create Invoice", link: "/create-invoice" },
+        { sublabel: "View Invoices", link: "/view-invoices" },
+        { sublabel: "Payments", link: "/payments" },
+        { sublabel: "Expenses", link: "/expenses" },
+        { sublabel: "Reports", link: "/reports" },
+      ],
+    },
+    {
+      label: "Inventory",
+      icon: "mdi mdi-cash-multiple",
+      subItem: [
+        { sublabel: "Inventory Table", link: "/inventory-table" },
+        { sublabel: "Table Form", link: "/add-inventory" },
+        { sublabel: "Table Taxation", link: "/table-taxation" },
+        { sublabel: "Item Config", link: "/item-configuration" },
+      ],
+    },
+{
+      label: "Authentication",
+      icon: "mdi mdi-account-circle-outline",
+      subItem: [
+        { sublabel: "Login", link: "/auth-login" },
+        { sublabel: "Register", link: "/auth-register" },
+        { sublabel: "Recover Password", link: "/recover-password" },
+        { sublabel: "Lock Screen", link: "/auth-lock-screen" },
+      ],
+    },
+    {
+        label: "Settings",
+        icon: "mdi mdi-cog-outline",
+        subItem: [
+          { sublabel: "Profile Settings", link: "/profile-settings" },
+          { sublabel: "Accounts Settings", link: "/accounts-setting" },
+          // { sublabel: "Recover Password", link: "/recover-password" },
+          // { sublabel: "Lock Screen", link: "/auth-lock-screen" },
+        ],
+      },
+      {
+        label: "Extra Pages",
+        icon: "mdi mdi-file-document-outline",
+        subItem: [
+          
+          { sublabel: "Ui Cards", link: "/ui-cards" },
+          { sublabel: "Ui Buttons", link: "/ui-buttons" },
+          { sublabel: "Ui Modals", link: "/ui-modals" },
+          { sublabel: "Boxicons", link: "/icon-boxicon" },
+          { sublabel: "MDI", link: "/icons-materialdesign" },
+          { sublabel: "FA-Icons", link: "/icons-fontawesome" },
+          { sublabel: "Drip-Icons", link: "/icon-dripicons" },
+        ],
+
+      }
+  ]
+
   export const accountantSidebarData = () => [
    
     {
@@ -266,8 +346,8 @@ export const masterAdminSidebarData = () => [
       case 'client_admin':
         return clientAdminSidebarData();
       case 'firm_admin':
-        return clientAdminSidebarData();
-        case 'accountant':
+        return firmAdminSidebarData();
+      case 'accountant':
           return accountantSidebarData();
       case 'readOnly':
         return readOnlySidebarData();
