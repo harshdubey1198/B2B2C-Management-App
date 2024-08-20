6c3c0b7aedba65885bb481a15a122f0184c902a0
rohitsaini27 <rohsaini8@gmail.com>
2024-08-20 14:50:01 +0530

    changes in schema for accountant firm employee

--- server/schemas/generalEmployee.schema.js
+++ server/schemas/generalEmployee.schema.js
@@ -2,26 +2,67 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const generalEmployeeSchema = new Schema({
-  cidm: { type: String, required: true },
-  fid: { type: String, required: true },
-  emp_id: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  avatar: { type: String, default: "https://res-console.cloudinary.com/harshdubey1198/media_explorer_thumbnails/e4538e487d236764e095b75071b82209/detailed"},
-  role: { type: String, required: true, enum: ['GeneralEmployee'] },
-  permissions: [{
-    route: { type: String, required: true },
-    sub_permissions: [{ type: String }]
-  }],
+  empId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firmId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Firms',
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: "https://res-console.cloudinary.com/harshdubey1198/media_explorer_thumbnails/e4538e487d236764e095b75071b82209/detailed"
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['GeneralEmployee']
+  },
+  permissions: {
+    type: Map,
+    of: [String],
+    default: {
+      routes: [],
+      apis: [],
+    },
+  },
+  status: {
+    type: String,
+    enum: ['Active', 'Inactive', 'Pending', 'Suspended'],
+    default: 'Active',
+  },
   logs: [{
     date: { type: Date, required: true },
     time: { type: String, required: true },
     page_history: [{ type: String }],
     ip_address: { type: String, required: true },
     country: { type: String, required: true }
-  }]
-}, {timestamps: true});
+  }],
+}, { timestamps: true });
 
 const GeneralEmployee = mongoose.model('GeneralEmployee', generalEmployeeSchema);
 
