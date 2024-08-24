afffba7938376cffdb9c9ff43145fde12cccce73
HARSH DUBEY <amanad08052@gmail.com>
2024-08-24 11:58:00 +0530

    updated firm address schema

--- server/schemas/firms.schema.js
+++ server/schemas/firms.schema.js
@@ -10,7 +10,15 @@ const firmsSchema = new Schema({
     firmPhone: {type: String, required: true},
     firmAdmin: {type: mongoose.Schema.Types.ObjectId, ref: "FirmAdmin"},
     avatar: { type: String, default: "https://res.cloudinary.com/harshdubey1198/image/upload/v1721475588/dummy-userImages/bqof59zlzkampcaxpws9.jpg"},
-    companyAddress: { type: String, required: true },
+    companyAddress: [{
+        h_no: { type: String, required: true },
+        nearby: { type: String, required: true },
+        district: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        zip_code: { type: String, required: true },
+        country: { type: String, required: true }
+      }],
     bankName: { type: String, required: true },
     accountNumber: { type: String, required: true },
     ifscCode: { type: String, required: true },
