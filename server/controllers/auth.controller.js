154d47638581157eaa3ca7538e199f4f8f79e66e
rohitsaini27 <rohsaini8@gmail.com>
2024-08-26 17:10:48 +0530

    changes in create user api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -37,4 +37,15 @@ router.post('/reset-password', async (req,res) => {
     })
 })
 
+router.post("/createUser", (req, res) => {
+      console.log(req.body, "body");
+    authServices.createUser(req.body).then((response) => {
+        res.status(200).send(response);
+    })
+    .catch((error) => {
+        console.log(error, "error creating users");
+        res.status(500).send(error);
+    });
+});
+
 module.exports = router
--- server/services/auth.services.js
+++ server/services/auth.services.js
@@ -15,6 +15,7 @@ let service = {};
 service.userLogin = userLogin;
 service.UserForgetPassword = UserForgetPassword;
 service.resetPassword = resetPassword;
+service.createUser = createUser;
 
 // LOGIN USER BASED ON THIER ROLE
 async function userLogin(body) {
@@ -228,6 +229,59 @@ async function resetPassword(body) {
       console.error(error);
       throw new Error('An error occurred while processing the request.');
   }
+}
+
+
+async function createUser(body) {
+    try {
+      const { role, firmUniqueId, email, password, ...rest } = body;
+      let uniqueIdField = "uid";
+  
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return Promise.reject("Email already exists");
+      }
+  
+      let uniqueId = "";
+      if (uniqueIdField) {
+        uniqueId = await generateUniqueId(
+            User,
+          uniqueIdField,
+          firmUniqueId,
+        );
+        rest[uniqueIdField] = uniqueId;
+      }
+  
+      const hashedPassword = await PasswordService.passwordHash(password);
+      // Create the new user
+      const newUser = new User({
+        ...rest,
+        email,
+        role,
+        password: hashedPassword,
+      });
+      await newUser.save();
+  
+      return newUser;
+    } catch (error) {
+      console.error("Error creating user:", error);
+      return Promise.reject(`Error creating user`);
+    }
+  }
+  
+async function generateUniqueId(User, uniqueIdField, firmUniqueId) {
+    let newUniqueId;
+    do {
+      const randomNumber = Math.floor(Math.random() * 1000).toString().padStart(3, "0");
+      newUniqueId = `${firmUniqueId}-${randomNumber}`;
+  
+      const existingId = await User.findOne({[uniqueIdField]: newUniqueId,});
+      if (!existingId) {
+        break;
+      }
+    } while (true);
+  
+    return newUniqueId;
 }
 
 module.exports = service;
