aae4708846d0c1f709d2cc2455ea93239bda737e
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2024-09-16 17:47:56 +0530

    integrate api in frontend create payment

--- server/routers/payment.router.js
+++ server/routers/payment.router.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const paymentController = require('../controllers/payment.controller')
+const paymentController = require('../controllers/payment.controller');
+const { tokenVerification } = require('../middleware/auth.middleware');
 
-router.post('/create-payment', paymentController.createPayment)
-router.get('/get-payment', paymentController.getPayment);
-router.put('/update-payment/:id', paymentController.updatePayment);
-router.delete('/delete-payment/:id', paymentController.deletePayment);
-
-
-
+router.post('/create-payment', tokenVerification, paymentController.createPayment)
+router.get('/get-payment', tokenVerification, paymentController.getPayment);
+router.put('/update-payment/:id', tokenVerification, paymentController.updatePayment);
+router.delete('/delete-payment/:id', tokenVerification, paymentController.deletePayment);
 
 module.exports = router
