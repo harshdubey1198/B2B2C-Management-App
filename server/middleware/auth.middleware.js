d0127ae2fcb8f452e56f57f7ba1f22046c6ee998
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2024-09-16 16:22:35 +0530

    solved token error in frontend

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -4,17 +4,18 @@ const utills = require("../utils/utills");
 function tokenVerification(req, res, next) {
   let token = req.header("Authorization");
   if (!token) {
-    return res.send(
-      utills.createResult("Token is required", null, "User is not logged in")
-    );
+    return res
+      .status(401)  
+      .send(utills.createResult("Token is required", null, "User is not logged in"));
   }
+  
   token = token.replace("Bearer ", "");
   try {
     jwt.verify(token, process.env.TOKEN_KEY, function (err, decoded) {
       if (err) {
-        return res.send(
-          utills.createResult("Invalid token", null, err.message)
-        );
+        return res
+          .status(401)  
+          .send(utills.createResult("Invalid token", null, err.message));
       }
       req.user = decoded;
       next();
@@ -26,30 +27,39 @@ function tokenVerification(req, res, next) {
   }
 }
 
-
 // SUPER ADMIN TOKEN VERIFICATION
 const superAdminTokenVerification = (req, res, next) => {
   let token = req.header('Authorization');
   if (!token) {
-    return res.send(utills.createResult("Token is required", null, "User is not logged in"));
+    return res
+      .status(401)  // Changed to 401 for Unauthorized
+      .send(utills.createResult("Token is required", null, "User is not logged in"));
   }
+  
   token = token.replace("Bearer ", "");
   try {
     jwt.verify(token, process.env.TOKEN_KEY, function (err, decoded) {
       if (err) {
-        return res.send(utills.createResult("Invalid token", null, err.message));
+        return res
+          .status(401)  
+          .send(utills.createResult("Invalid token", null, err.message));
       }
       if (decoded.role !== 'super_admin') {
-        return res.send(utills.createResult("Access Denied", null, "You are not a super admin"));
+        return res
+          .status(403) 
+          .send(utills.createResult("Access Denied", null, "You are not a super admin"));
       }
       req.user = decoded;
       next();
     });
   } catch (err) {
-    return res.status(400).send(utills.createResult(err.message, null, "Something went wrong"));
+    return res
+      .status(400)
+      .send(utills.createResult(err.message, null, "Something went wrong"));
   }
 };
 
 module.exports = {
-  tokenVerification, superAdminTokenVerification
+  tokenVerification, 
+  superAdminTokenVerification
 };
