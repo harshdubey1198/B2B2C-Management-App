66fd6919bad10bd564bf6d2c9e35ce83bfada9ac
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2024-09-18 13:25:34 +0530

    create api for category

--- server/services/category.services.js
+++ server/services/category.services.js
@@ -36,5 +36,22 @@ categoryServices.getCategory = async () => {
     return data
 }
 
+//  UPDATE CATEGORY
+categoryServices.updateCategory = async (id, body) => {
+    const {parentId} = body
+    const existingCategory = await Category.findOne({_id:id})
+    if(!existingCategory){
+        throw new Error('Category does not exist')
+    }
+    if (parentId && parentId !== id) {
+        const parentCategory = await Category.findById(parentId);
+        if (!parentCategory) {
+            throw new Error('Parent category does not exist');
+        }
+    }
+
+    const updateCategory = await Category.findByIdAndUpdate(id, body, {new:true})
+    return updateCategory
+} 
 
 module.exports = categoryServices;
