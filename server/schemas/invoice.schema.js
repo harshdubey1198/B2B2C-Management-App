0f7082eb96aaaf46882395f5791149bc533bcd10
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2024-09-24 16:12:09 +0530

    working on schemas of invoices

--- server/schemas/invoice.schema.js
+++ server/schemas/invoice.schema.js
@@ -3,7 +3,16 @@ const Schema = mongoose.Schema;
 
 const invoiceSchema = new Schema({
     invoiceNumber: { type: String, required: true },
-    customerId: { type: Schema.Types.ObjectId, ref: 'Customer',}, 
+    customerName: { type: String, required: true },     
+    customerEmail: { type: String, required: true },    
+    customerPhone: { type: String },                     
+    customerAddress: {                                  
+        h_no: { type: String },
+        city: { type: String },
+        state: { type: String },
+        zip_code: { type: String },
+        country: { type: String }
+    },
     firmId: { type: Schema.Types.ObjectId, ref: 'User' }, 
     items: [{
         itemId: { type: Schema.Types.ObjectId, ref: 'InventoryItem' },
