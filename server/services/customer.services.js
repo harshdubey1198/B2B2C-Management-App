398c087831a82821b026fc15b5d517c1ef92fc95
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2024-09-26 14:30:07 +0530

    ceate invoice api and search api

--- server/services/customer.services.js
+++ server/services/customer.services.js
@@ -3,6 +3,7 @@ const Customer = require("../schemas/cutomer.schema");
 
 const customerServices = {};
 
+// SEARCH FOR A CUSTOMER UNDER THAT FIRM
 customerServices.searchCustomer = async (searchQuery, firmId) => {
   if (!searchQuery || !firmId) {
     throw new Error("Search query and firmId are required");
@@ -19,5 +20,46 @@ customerServices.searchCustomer = async (searchQuery, firmId) => {
   }
   return customer;
 };
+
+// GET ALL CUSTOMERS UNDER THAT FIRM
+customerServices.getAllCustomers = async (firmId) => {
+  if (!firmId) {
+    throw new Error("firmId is required");
+  }
+  const customers = await Customer.find({firmId: firmId})
+  .populate({
+    path: "firmId",
+    select: "companyTitle email avatar",
+  })
+  .populate({
+    path: "createdBy",
+    select: "firstName lastName email",
+  });
+
+  if(customers.length === 0){
+    throw new Error("No Customers found for this firms")
+  }   
+  return customers
+}
+
+// GET SINGLE CUSTOMER SUCCEFULLY
+customerServices.getCustomer = async (customerId) => {
+  if (!customerId) {
+    throw new Error("customerId is required");
+  }
+  const customer = await Customer.find({_id: customerId})
+  .populate({
+    path: "firmId",
+    select: "companyTitle email avatar",
+  })
+  .populate({
+    path: "createdBy",
+    select: "firstName lastName email",
+  });
+  if(!customer){
+    throw new Error("Customer Not Found")
+  }
+  return customer
+}
 
 module.exports = customerServices;
