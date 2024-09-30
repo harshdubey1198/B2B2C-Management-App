d464841b39e1ba260bb533aa55f157615172be52
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2024-09-30 15:25:58 +0530

    solved variant error in frontend

--- src/components/InvoicingComponents/InvoiceItems.jsx
+++ src/components/InvoicingComponents/InvoiceItems.jsx
@@ -110,6 +110,8 @@ const InvoiceItems = ({ items, handleItemChange, removeItem, setInvoiceData }) =
     }
   };
 
+  console.log(items, "items")
+
   if (loading) return <Spinner color="primary" />;
 
   return (
