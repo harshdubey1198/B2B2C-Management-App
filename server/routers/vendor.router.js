12407adf7f3320220cf314d135720c40be9cea70
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2024-10-21 13:16:58 +0530

    create api for reject invoice and create api for create vendors

--- server/routers/vendor.router.js
+++ server/routers/vendor.router.js
@@ -4,7 +4,7 @@ const vendorController = require('../controllers/vendor.controller');
 const { tokenVerification } = require('../middleware/auth.middleware');
 
 router.post('/create-vendor/:id', tokenVerification, vendorController.createVendor);
-router.get('/get-vendors/:id', vendorController.getVendors);
+router.get('/get-vendors/:id',tokenVerification, vendorController.getVendors);
 router.get('/get-vendor/:id', tokenVerification, vendorController.getVendor);
 router.put('/update-vendor/:id', tokenVerification, vendorController.updateVendor);
 router.delete('/delete-vendor/:id', tokenVerification, vendorController.deleteVendor);
