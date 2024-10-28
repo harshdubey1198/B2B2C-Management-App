7948a7c10474b70befb2c5c975619559fc6e869c
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2024-10-28 14:36:52 +0530

    working on view format pages

--- src/Modal/ItemDetailModal.jsx
+++ src/Modal/ItemDetailModal.jsx
@@ -80,7 +80,7 @@ const ItemDetailModal = ({ setVariantIndex, setVariant, setVariantModalOpen, set
                 <label>
                   <strong>Vendor:</strong>
                 </label>
-                <input type="text"   value={selectedItem?.vendor?.name ?? 'N/A'} className="form-control" readOnly />
+                <input type="text" value={selectedItem.vendor.name} className="form-control" readOnly />
               </Col>
               {/* <Col md={3}>
                 <label>
@@ -232,6 +232,7 @@ const ItemDetailModal = ({ setVariantIndex, setVariant, setVariantModalOpen, set
                     })
                   }
                   className="form-control"
+                  readOnly
                 />
               </Col>
               <Col md={6}>
@@ -248,6 +249,7 @@ const ItemDetailModal = ({ setVariantIndex, setVariant, setVariantModalOpen, set
                     })
                   }
                   className="form-control"
+                  readOnly
                 />
               </Col>
             </Row>
@@ -267,6 +269,7 @@ const ItemDetailModal = ({ setVariantIndex, setVariant, setVariantModalOpen, set
                     })
                   }
                   className="form-control"
+                  readOnly
                 />
               </Col>
             </Row>
