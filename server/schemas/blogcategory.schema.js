e43ba998b8490ce014e1b5615a5af34f220e9767
Rohit Saini <129052651+rohitsaini27@users.noreply.github.com>
2025-01-10 12:07:57 +0530

    create category

---
+++ server/schemas/blogcategory.schema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const blogCategorySchema = new mongoose.Schema(
+  {
+    categoryName: { type: String, required: true},
+    slug: { type: String, required: true, unique: false,},
+    status: { type: String, enum: ["active", "inactive"], default: "active",},
+    parentId: { type: mongoose.Schema.Types.ObjectId, ref: "blogCategory", default: null,},
+    deleted_at: { type: Date, default: null }
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const BlogCategory = mongoose.model('BlogCategory', blogCategorySchema);
+module.exports = BlogCategory;
