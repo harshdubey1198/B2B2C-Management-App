c0f6ca954df658ef331f59af0fb8410f24927504
HARSH DUBEY <amanad08052@gmail.com>
2025-03-01 13:55:20 +0530

    po page 5 item per page from 10

--- src/Pages/Production-Inventory/ProductionOrders.jsx
+++ src/Pages/Production-Inventory/ProductionOrders.jsx
@@ -177,7 +177,7 @@ const handleCreateModal = () => {
                     style={{ width: "auto" ,maxHeight:"30px" , padding:"2px 24px"}}
                     onChange={handleItemsPerPageChange}
                   >
-                    <option value="10" >10</option>
+                    <option value="5" >5</option>
                     <option value="25" >25</option>
                     <option value="50" >50</option>
                     <option value="70" >70</option>
